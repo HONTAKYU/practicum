ca1fdb6b736a6eb7305558c7ee930dcd3df6e189
HONTAKYU <HDYhandeyu@gmail.com>
2017-09-23 01:47:07 -0700

    fix issue with path in build.sh in Modsecurity

--- install_with_WAF_v3.sh
+++ install_with_WAF_v3.sh
@@ -10,7 +10,8 @@ apt-get install sudo wget vim git zlib1g-dev apache2-dev libcurl4-gnutls-dev lib
 git clone https://github.com/SpiderLabs/ModSecurity
 cd ModSecurity/
 git checkout -b v3/master origin/v3/master
-sh build.sh
+mv ../ltmain.sh .
+./build.sh
 git submodule init
 git submodule update #[for bindings/python, others/libinjection, test/test-cases/secrules-language-tests]
 ./configure
